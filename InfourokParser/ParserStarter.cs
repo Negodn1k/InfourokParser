26beda2a24d79c3d0adbb8c732aaf247afd9cf75
Ivan <Ivan@DESKTOP-MBHAQCI>
2026-09-10 19:12:08 +0300

    Добавьте файлы проекта.

---
+++ InfourokParser/ParserStarter.cs
@@ -0,0 +1,36 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace InfourokParser
+{
+    internal class ParserStarter
+    {
+        public void GetText(string urlAddr)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(urlAddr);
+            IEnumerable<HtmlNode> nodes = htmlDoc.DocumentNode.Descendants(0).Where(n => n.HasClass("MsoNormal"));
+
+            using (WordprocessingDocument wordprocessing = WordprocessingDocument.Create("parsedText.docx", WordprocessingDocumentType.Document))
+                {
+
+                MainDocumentPart mainDocPart = wordprocessing.AddMainDocumentPart();
+                mainDocPart.Document = new Document();
+                mainDocPart.Document.Body = mainDocPart.Document.AppendChild(new Body());
+                Body body = mainDocPart.Document.Body;
+                foreach (HtmlNode node in nodes)
+                {
+                    Paragraph para = body.AppendChild(new Paragraph());
+                    Run run = para.AppendChild(new Run());
+                    Text text = run.AppendChild(new Text(node.InnerText.Replace("&nbsp;", " ")));
+                }
+            }
+        } 
+    }
+}
